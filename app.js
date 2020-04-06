9349d157e9f35d4db78fab371998ea8cd6d44465
AdeelApple <M.AdeelApple@gmail.com>
2020-04-06 05:46:40 +0500

    Upadated

---
+++ app.js
@@ -0,0 +1,11 @@
+require('./cartbox')
+// import { getRs } from './cartbox';
+
+// console.log(getcart.hellow)
+// getcart.brown(2000)
+// console.log('Hellow World???? how is lockdown????')
+
+
+// cart(2000)
+cart1(2000)
+
---
+++ cartbox.js
@@ -0,0 +1,14 @@
+const getRs = 5000;
+console.log("Module Imported!")
+// getRs = 600;
+cart = (rs) => {
+	console.log(rs-200)
+}
+cart1 = (rs) => {
+	console.log(rs-500)
+}
+// cart(getRs)
+
+// exports.brown = cart1
+
+// export getRs
